f3ec101beb1ba3a54447d2bb2b7af9fc0350fcd0
ariinromeror <ariin.romeror@gmail.com>
2026-02-05 18:10:32 +0100

    FIX: permitiendo que el front use variables de entorno

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext(null);
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
