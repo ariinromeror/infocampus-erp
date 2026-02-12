e60c9ac1b87507697e7e06f51cae03af938264fc
ariinromeror <ariin.romeror@gmail.com>
2026-02-12 10:43:28 +0100

    Fix trailing slash in login

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
 
 const login = async (username, password) => {
     try {
-        const authResponse = await fetch(`${API_URL}/login`, {  
+        const authResponse = await fetch(`${API_URL.replace(/\/$/, '')}/login`, {  
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
